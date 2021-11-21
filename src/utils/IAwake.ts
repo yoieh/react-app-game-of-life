337f1b1774f8be8b81d18fd098fa807169b0550b
Yoieh <johan.nilsson@nerdsofsweden.se>
2021-11-21 14:59:31 +0100

    moved ecs to utils

---
+++ src/utils/IAwake.ts
@@ -0,0 +1,3 @@
+export interface IAwake {
+  Awake(): void;
+}
---
+++ src/utils/engine/Engine.ts
@@ -0,0 +1,26 @@
+import { Entity } from "../ECS/Entity";
+
+class Engine extends Entity {
+  private lastTimestamp = 0;
+
+  constructor() {
+    super();
+    // start update loop
+    this.Update();
+  }
+
+  public Update(): void {
+    const deltaTime = (Date.now() - this.lastTimestamp) / 1000;
+
+    // update all components
+    super.Update(deltaTime);
+
+    // update the timestamp
+    this.lastTimestamp = Date.now();
+
+    // Invoke on next frame
+    requestAnimationFrame(() => this.Update());
+  }
+}
+
+export default Engine;
